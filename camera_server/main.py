b1c8d8e77a84f837f1be5d292bf1ad59eda5ad5e
MunoLike <sukorice@gmail.com>
2022-11-01 08:20:01 +0900

    動かしてみた

--- camera_server/main.py
+++ camera_server/main.py
@@ -1,17 +1,32 @@
-from cgitb import html
-from fastapi import FastAPI, WebSocket,WebSocketDisconnect
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, WebSocketDisconnect,Request
+from starlette.websockets import WebSocket
+from fastapi.templating import Jinja2Templates
 import uvicorn
 import cv2
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="./", html=True), name="static")
+templates = Jinja2Templates(directory="static")
 
 camera = cv2.VideoCapture("test.mp4")
+
+
+@app.get("/")
+async def home(req: Request):
+    return templates.TemplateResponse("index.html", {"request":req})
 
 @app.websocket("/vidws/")
 async def get_stream(ws:WebSocket):
     await ws.accept()
+    try:
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                await ws.send_bytes(buffer.tobytes())
+    except WebSocketDisconnect:
+        print("Client disconnected")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
