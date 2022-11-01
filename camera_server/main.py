10112cca770cd0f632004403d75df4d81253f433
MunoLike <sukorice@gmail.com>
2022-11-01 18:09:21 +0900

    なぜかMJPG出力に切り替えられない(Windows)

--- camera_server/main.py
+++ camera_server/main.py
@@ -7,7 +7,13 @@ import cv2
 app = FastAPI()
 templates = Jinja2Templates(directory="static")
 
-camera = cv2.VideoCapture("test.mp4")
+camera = cv2.VideoCapture(0)
+camera.set(cv2.CAP_PROP_FOURCC ,cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+camera.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+print((int(camera.get(cv2.CAP_PROP_FOURCC)).to_bytes(4, 'little').decode('utf-8')))
 
 
 @app.get("/")
@@ -23,10 +29,11 @@ async def get_stream(ws:WebSocket):
             if not success:
                 break
             else:
-                ret, buffer = cv2.imencode('.jpg', frame)
-                await ws.send_bytes(buffer.tobytes())
+                # ret, buffer = cv2.imencode('.jpg', frame)
+                await ws.send_bytes(frame.tobytes())
     except WebSocketDisconnect:
         print("Client disconnected")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
+    cv2.imwrite()
